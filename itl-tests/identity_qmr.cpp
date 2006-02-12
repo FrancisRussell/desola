fa4472bd4627047d25079cf4482390979d197e80
Francis Russell <fpr02@doc.ic.ac.uk>
2006-02-12 15:43:09 +0000

    Added identity_qmr itl test.
    Monotone-Parent: 2527ca7cff8c16b59aa24f2aae00ab5501771940
    Monotone-Revision: 90dce9bebe5504e56535c601ed8ae21e405501fa

---
+++ itl-tests/identity_qmr.cpp
@@ -0,0 +1,117 @@
+//===========================================================================
+//  CVS Information:                                                         
+//                                                                           
+//     $RCSfile: ilu_qmr.cpp,v $  $Revision: 1.3 $  $State: Exp $ 
+//     $Author: llee $  $Date: 2001/10/18 14:08:33 $ 
+//     $Locker:  $ 
+//---------------------------------------------------------------------------
+//                                                                           
+// DESCRIPTION                                                               
+//                                                                           
+//---------------------------------------------------------------------------
+//                                                                           
+// LICENSE AGREEMENT                                                         
+// Copyright 1997, University of Notre Dame.
+// Authors: Andrew Lumsdaine, Lie-Quan Lee
+//
+// This file is part of the Iterative Template Library
+//
+// You should have received a copy of the License Agreement for the
+// Iterative Template Library along with the software;  see the
+// file LICENSE.  If not, contact Office of Research, University of Notre
+// Dame, Notre Dame, IN  46556.
+//
+// Permission to modify the code and to distribute modified code is
+// granted, provided the text of this NOTICE is retained, a notice that
+// the code was modified is included with the above COPYRIGHT NOTICE and
+// with the COPYRIGHT NOTICE in the LICENSE file, and that the LICENSE
+// file is distributed with the modified code.
+//
+// LICENSOR MAKES NO REPRESENTATIONS OR WARRANTIES, EXPRESS OR IMPLIED.
+// By way of example, but not limitation, Licensor MAKES NO
+// REPRESENTATIONS OR WARRANTIES OF MERCHANTABILITY OR FITNESS FOR ANY
+// PARTICULAR PURPOSE OR THAT THE USE OF THE LICENSED SOFTWARE COMPONENTS
+// OR DOCUMENTATION WILL NOT INFRINGE ANY PATENTS, COPYRIGHTS, TRADEMARKS
+// OR OTHER RIGHTS.
+//---------------------------------------------------------------------------
+//                                                                           
+// REVISION HISTORY:                                                         
+//                                                                           
+// $Log: ilu_qmr.cpp,v $
+// Revision 1.3  2001/10/18 14:08:33  llee
+// re-organize the directory structures
+//
+// Revision 1.2  2000/07/27 04:39:21  llee1
+// *** empty log message ***
+//
+// Revision 1.1  2000/07/26 21:50:09  llee1
+// change file extension from .cc to .cpp
+//
+// Revision 1.1  2000/07/18 16:41:01  llee1
+// add Harwell-Boeing matrix test
+//
+// Revision 1.2  2000/07/17 15:44:04  llee1
+// *** empty log message ***
+//
+// Revision 1.1  1999/11/21 20:47:07  lums
+// *** empty log message ***
+//
+//===========================================================================
+
+#include <desolin/Desolin.hpp>
+#include <itl/interface/mtl.h>
+#include "itl/preconditioner/ilu.h"
+#include "itl/krylov/qmr.h"
+
+/*
+  In thsi example, we show how to use QMR algorithm
+*/
+using namespace itl;
+
+typedef double Type;
+typedef desolin::Scalar<Type> Scalar;
+typedef desolin::Vector<Type> Vector;
+typedef desolin::Matrix<Type> Matrix;
+
+int main(int argc, char* argv[])
+{
+  using std::cout;
+  using std::endl;
+
+  if ( argc == 1 ) {
+    cout << "Usage: " << argv[0] 
+	 << " <Unsymmetric matrix in Harwell-Boeing format> "
+	 << endl;
+    return 0;
+  }
+
+  harwell_boeing_stream<Type> hbs(argv[1]);
+
+  int max_iter = 50;
+  //begin
+  Matrix A(hbs);
+  //end
+
+  //begin
+  Vector x(A.nrows(), Type(0));
+  Vector b(A.ncols(), Type(1.));
+
+  //Identity preconditioner
+  identity_preconditioner precond;
+  //iteration
+  noisy_iteration<Scalar> iter(b, max_iter, 1e-6);
+  //qmr algorithm
+  qmr(A, x, b, precond.left(), precond.right(), iter);
+  //end
+
+  //verify the result
+  Vector b1(A.ncols());
+  itl::mult(A, x, b1);
+  itl::add(b1, itl::scaled(b, -1.), b1);
+  
+  cout << "Residual " << itl::two_norm(b1) << endl;
+
+  return 0;
+}
+
+
