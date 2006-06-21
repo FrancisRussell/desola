969dd30f2af82e2a5d544c552625807deb60ab76
Francis Russell <fpr02@doc.ic.ac.uk>
2006-06-21 17:12:04 +0000

    Added licenses to most files.
    Monotone-Parent: 9dcb35b493a608aac145022ce547eb723a57f839
    Monotone-Revision: 58b0b417c660c38672bb4636814bfd2f3664e60c

--- example/test.cpp
+++ example/test.cpp
@@ -15,27 +15,38 @@
 /*                                                                          */
 /****************************************************************************/
 
-#ifndef DESOLIN_TG_HPP
-#define DESOLIN_TG_HPP
+#include <desolin/Desolin.hpp>
+#include <TaskGraph>
+#include <vector>
+#include <iostream>
+using namespace desolin;
 
-#include "Exceptions.hpp"
-#include "Traits.hpp"
-#include "ExpressionNode.hpp"
-#include "BinOp.hpp"
-#include "UnOp.hpp"
-#include "Elemental.hpp"
-#include "Literal.hpp"
-#include "MatrixVector.hpp"
-#include "Pairwise.hpp"
-#include "ScalarPiecewise.hpp"
-#include "ExpressionNodeVisitor.hpp"
-#include "HashingVisitor.hpp"
-#include "EqualityCheckingVisitor.hpp"
-#include "Objects.hpp"
-#include "ExpressionGraph.hpp"
-#include "Evaluator.hpp"
-#include "CodeGenerator.hpp"
-#include "ObjectGenerator.hpp"
-#include "ObjectGeneratorHelper.hpp"
+int main()
+{
+  Scalar<double> aa = -5.4;
+  Scalar<double> bb = -12.2;
 
-#endif
+  std::cout << std::abs(aa + bb) << std::endl;
+  
+  Scalar<double> x(10);
+  Scalar<double> y(44);
+  Scalar<double> c = x+y;
+  c = c+c;
+  std::cout << c << std::endl;
+  Vector<double> q(5, 4);
+  Vector<double> r = q*c;
+  std::cout << r.norm() << std::endl;
+
+  Matrix<double> m1(5,4, 0);
+  Matrix<double> m2(4,7, 0);
+  Matrix<double> m3 = m1*m2;
+  std::cout << "m3(1,1) = " << m3(1,1).value() << std::endl;
+  m3(1,1) = c;
+  std::cout << "m3 = " << m3 << std::endl;
+  std::cout << "-m3 = " << -m3 << q << std::endl;
+  std::vector< Scalar<double> > hello;
+  hello.push_back(m3(1,1) = c);
+
+  harwell_boeing_stream<double> hbstream("../test-matrices/bcsstk14.rsa");
+  Matrix<double> hb(hbstream);
+}
